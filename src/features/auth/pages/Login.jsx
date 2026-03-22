d8a4e22d57a0dc271688b4458a161b0822657dc8
Aditi Shukla <aditishukla1676@gmail.com>
2026-03-22 23:46:23 +0530

    authentication part done

---
+++ src/features/auth/pages/Login.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useNavigate } from 'react-router'
+import { Link } from 'react-router'
+import "./auth.form.scss"
+const Login = () => {
+
+    const  navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+     
+
+  return (
+   <main>
+    <div className="form-container">
+        <h1>Login</h1>
+
+        <form onSubmit ={handleSubmit}>
+
+            <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" id = "email" name='email' placeholder='Enter email address' />
+            </div>
+            <div className="input-group">
+                <label htmlFor="password">Password</label>
+                 <input type="password" id = "password" name='password' placeholder='Enter password' />
+            </div>
+
+            <button className='button primary-button'>Login</button>
+        </form>
+          <p>Don't have an account?<Link to ={"/register"}>Register</Link></p>
+        </div>
+   </main>
+  )
+}
+
+export default Login
